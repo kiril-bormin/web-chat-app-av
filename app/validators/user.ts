58b1335bfee212cba30154314f503ec8ae4591fa
Kiril Bormin <borminnkiril@gmail.com>
2026-04-21 08:47:52 +0200

    add validateurs

--- app/validators/user.ts
+++ app/validators/user.ts
@@ -4,7 +4,7 @@ const email = () => vine.string().email().maxLength(254)
 const password = () => vine.string().minLength(8).maxLength(32)
 
 export const signupValidator = vine.create({
-  fullName: vine.string().nullable(),
+  fullName: vine.string().trim().minLength(3),
   email: email().unique({ table: 'users', column: 'email' }),
   password: password().confirmed({
     confirmationField: 'passwordConfirmation',
